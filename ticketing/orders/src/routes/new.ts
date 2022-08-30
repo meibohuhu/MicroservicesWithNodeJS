1c733e02f5acbfb13f7dcf7ae3e545735bd0c0dd
Wendy Hu <meibohu96@gmail.com>
2022-08-29 22:32:20 -0700

    Section17 Cross-Service data
    Order service: has ticket and order two models
    Four APIs

---
+++ ticketing/orders/src/routes/new.ts
@@ -0,0 +1,57 @@
+import {   requireAuth,
+  validateRequest,
+  NotFoundError,
+  OrderStatus,
+  BadRequestError, } from '@wendy96tickets/common';
+import express, { Request, Response } from 'express';
+import mongoose from 'mongoose';
+import { body } from 'express-validator';
+import { Ticket } from '../models/ticket';
+import { Order } from '../models/order';
+
+const router = express.Router();
+const EXPIRATION_WINDOW_SECONDS = 15 * 60;
+
+router.post(
+  '/api/orders', 
+  requireAuth,
+  [
+    body('ticketId')
+    .not()
+    .isEmpty()
+    .custom((input: string) => mongoose.Types.ObjectId.isValid(input))
+    .withMessage('TicketId must be provided'),
+  ],
+  validateRequest,
+  async (req: Request, res: Response) => {
+    const { ticketId } = req.body;
+    // 1. Find the ticket the user is trying to order in the database
+    const ticket = await Ticket.findById(ticketId);
+    if (!ticket) {
+      throw new NotFoundError();
+    }
+
+    // 2. Make sure this ticket is not already reserved
+    const isReserved = await ticket.isReserved();
+    if (isReserved) {
+      throw new BadRequestError('Ticket is already reserved');
+    }
+
+    // 3. Calculate an expiration date for this order
+    const expiration = new Date();
+    expiration.setSeconds(expiration.getSeconds() + EXPIRATION_WINDOW_SECONDS);
+    
+    // 4. Build the order and save it to the database
+    const order = Order.build({
+      userId: req.currentUser!.id,
+      status: OrderStatus.Created,
+      expiresAt: expiration,
+      ticket,
+    });
+    await order.save();
+    
+    res.status(201).send(order);
+  }
+);
+
+export { router as newOrderRouter };
